c4830c11da031d42c7911861851ed752f7921a4f
Arina <arina.buly4eva@yandex.ru>
2024-04-19 11:17:43 +0300

    perehogy go forgot

--- app/src/main/java/com/example/lol/MainActivity3.java
+++ app/src/main/java/com/example/lol/MainActivity3.java
@@ -7,6 +7,14 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity3 extends AppCompatActivity {
 
     @Override
@@ -23,4 +31,17 @@ public class MainActivity3 extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-}}
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Здесь может быть ваша логика обработки данных формы
+                Toast.makeText(MainActivity3.this, "Button clicked", Toast.LENGTH_SHORT).show();
+                // Открытие activity_log_in.xml
+                Intent intent = new Intent(MainActivity3.this, LogIn.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
